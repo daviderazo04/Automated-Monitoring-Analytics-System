8d405b1bafe967ab84135b781c6663fdb33811b6
David Erazo <daviderazo079@gmail.com>
2026-02-03 21:28:04 -0500

    Initial commit: Automated Monitoring & Analytics System base structure

---
+++ src/main/java/com/daviderazo04/automated_monitoring_system/Application.java
@@ -0,0 +1,13 @@
+package com.daviderazo04.automated_monitoring_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
