53918882a5b135599e6dbac7c42efb1d8d0335a3
David Erazo <daviderazo079@gmail.com>
2026-02-25 21:54:14 -0500

    temporarily permit all requests for principal functionality testing

---
+++ src/main/java/com/daviderazo04/automated_monitoring_system/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.daviderazo04.automated_monitoring_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                // Esta línea asegura que CUALQUIER petición pase sin pedir login
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
